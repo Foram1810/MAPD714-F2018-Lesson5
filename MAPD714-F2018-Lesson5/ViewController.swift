8449500fd69999c710d778ebc0d50dd4a57b7262
Foram Patel <fpatel1810@gmail.com>
2018-10-04 18:56:16 -0400

    Initial Commit

---
+++ MAPD714-F2018-Lesson5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714-F2018-Lesson5
+//
+//  Created by newuser on 2018-10-04.
+//  Copyright © 2018 newuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MAPD714-F2018-Lesson5Tests/MAPD714_F2018_Lesson5Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MAPD714_F2018_Lesson5Tests.swift
+//  MAPD714-F2018-Lesson5Tests
+//
+//  Created by newuser on 2018-10-04.
+//  Copyright © 2018 newuser. All rights reserved.
+//
+
+import XCTest
+@testable import MAPD714_F2018_Lesson5
+
+class MAPD714_F2018_Lesson5Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
